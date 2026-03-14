ffa3f25f63339aaadd14e4810c9c25113b9e7bef
AzeGamer1 <129595870+AzeGamer1@users.noreply.github.com>
2026-03-14 14:37:06 +0400

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$directory = './'; // Dosyaların bulunduğu dizin
-
-$files = array_diff(scandir($directory), array('.', '..')); // '.' ve '..' hariç tüm dosyaları al
-
-echo json_encode($files, JSON_PRETTY_PRINT); // Dosya isimlerini JSON formatında göster
-
-?>
